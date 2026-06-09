e61b29342edf7fe2c9c2710680e0db80f0f87959
ahmer <ahmerbilalsahi786@gmail.com>
2026-06-09 13:28:04 +0500

    Remove unused import for setAuthTokenGetter

--- artifacts/mohalla/src/main.tsx
+++ artifacts/mohalla/src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { setAuthTokenGetter } from "@workspace/api-client-react";
 import { getToken } from "./lib/auth";
 
 // Wire the auth token into every API call made via customFetch
